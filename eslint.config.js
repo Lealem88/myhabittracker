c15c87553ee46d512d21a72b22e5f34f29fea3e7
laliproj <lalitechzone@gmail.com>
2026-01-07 12:30:57 +0300

    optimized eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,3 +6,11 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
